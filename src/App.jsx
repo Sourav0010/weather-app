0537b21027f7e4fe9e97e5cfc45175af0520cad3
Sourav0010 <mohantysourav656@gmail.com>
2024-06-25 12:01:14 +0530

    Basic fetch done

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
            <p>{time}</p>
         </div>
         <div className='flex items-center gap-2'>
-            <input type="text" name="price" id="price" className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-blue-300 placeholder:text-gray-400 text-blue-600 focus:ring-blue-400 sm:text-sm sm:leading-6" placeholder="Enter Your city here"></input>
+            <input type="text" name="price" id="price" className="block w-full rounded-md border-0 py-1.5 pl-2 pr-4 text-gray-900 ring-1 ring-inset ring-blue-300 placeholder:text-gray-400 text-blue-600 focus:ring-blue-400 sm:text-sm sm:leading-6" placeholder="Enter Your city here"></input>
             <button 
             className=' text-blue-700 bg-blue-50  hover:bg-blue-100 py-2 px-4 rounded-md rounded-sm search' onClick={searchCity}>
               Search
